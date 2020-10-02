61aee9dec21fd33980f0b3fc57a26729a2e4c252
Sergey <72165027+sglushkov23@users.noreply.github.com>
2020-10-03 01:53:05 +0700

    Update Range.java

--- Range/src/ru/academits/java/glushkov/range/Range.java
+++ Range/src/ru/academits/java/glushkov/range/Range.java
@@ -2,6 +2,6 @@ package ru.academits.java.glushkov.range;
 
 public class Range {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world 2!");
     }
 }
