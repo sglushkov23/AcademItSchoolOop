fa49a453004cb4cebf38121055a94a5f7582d825
sglushkov23 <sglushkov23@gmail.com>
2020-11-29 16:03:34 +0700

    Correct ArrayListHome module

--- ArrayListHome/src/ru/academits/java/glushkov/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/java/glushkov/array_list_home/ArrayListHome.java
@@ -1,58 +1,83 @@
 package ru.academits.java.glushkov.array_list_home;
 
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) {
-        System.out.println("1)");
-        ArrayList<String> list1 = new ArrayList<>();
-
-        try (Scanner scanner = new Scanner(new FileInputStream("input.txt"))) {
-            while (scanner.hasNext()) {
-                list1.add(scanner.nextLine());
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла");
+    public static void readFileLinesToArrayList(String fileName, ArrayList<String> list) {
+        if (list == null) {
+            throw new NullPointerException("List argument must be not null");
         }
 
-        System.out.println("list1 = " + list1);
+        try (Scanner scanner = new Scanner(new FileInputStream(fileName))) {
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 
-        System.out.println();
-        System.out.println("2)");
+    public static void removeEvenNumbersFromList(ArrayList<Integer> list) {
+        if (list == null) {
+            throw new NullPointerException("List argument must be not null");
+        }
 
-        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 5, 6, 7, 8, 8, 9));
-        System.out.println("Исходный список:");
-        System.out.println("list2 = " + list2);
-
-        for (int i = 0; i < list2.size(); i++) {
-            if (list2.get(i) % 2 == 0) {
-                list2.remove(i);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0) {
+                list.remove(i);
                 i--;
             }
         }
+    }
 
-        System.out.println();
-        System.out.println("Список после удаления четных чисел:");
-        System.out.println("list2 = " + list2);
+    public static <T> ArrayList<T> getListWithoutDuplicates(ArrayList<T> list) {
+        if (list == null) {
+            throw new NullPointerException("List argument must be not null");
+        }
 
-        System.out.println();
-        System.out.println("3)");
+        ArrayList<T> listWithoutDuplicates = new ArrayList<>(list.size());
 
-        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
-        ArrayList<Integer> list4 = new ArrayList<>();
-
-        System.out.println("Список с повторениями list3 = " + list3);
-
-        for (Integer item : list3) {
-            if (!list4.contains(item)) {
-                list4.add(item);
+        for (T item : list) {
+            if (!listWithoutDuplicates.contains(item)) {
+                listWithoutDuplicates.add(item);
             }
         }
 
-        System.out.println("Список без повторений list4 = " + list4);
+        return listWithoutDuplicates;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1) Чтение в список всех строк из файла");
+
+        ArrayList<String> fileLinesList = new ArrayList<>();
+        readFileLinesToArrayList("input.txt", fileLinesList);
+        System.out.println("fileLinesList = " + fileLinesList);
+
+        System.out.println();
+        System.out.println("2) Удаление из списка всех четных чисел");
+
+        ArrayList<Integer> listToRemoveEvenNumbers = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 5, 6, 7, 8, 8, 9));
+
+        System.out.println("Исходный список до удаления четных чисел:");
+        System.out.println(listToRemoveEvenNumbers);
+
+        removeEvenNumbersFromList(listToRemoveEvenNumbers);
+
+        System.out.println();
+        System.out.println("Список после удаления четных чисел:");
+        System.out.println(listToRemoveEvenNumbers);
+
+        System.out.println();
+        System.out.println("3) Создание списка без повторений");
+
+        ArrayList<Integer> listWithDuplicates = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
+        ArrayList<Integer> listWithoutDuplicates = getListWithoutDuplicates(listWithDuplicates);
+
+        System.out.println("Исходный список с повторениями = " + listWithDuplicates);
+        System.out.println("Список без повторений = " + listWithoutDuplicates);
     }
 }
