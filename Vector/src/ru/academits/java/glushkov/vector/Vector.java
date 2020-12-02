033830938ec2e78d7aa76fbce4ea1d133c34782d
sglushkov23 <sglushkov23@gmail.com>
2020-12-03 02:17:23 +0700

    Correct Vector class

--- Vector/src/ru/academits/java/glushkov/vector/Vector.java
+++ Vector/src/ru/academits/java/glushkov/vector/Vector.java
@@ -3,14 +3,14 @@ package ru.academits.java.glushkov.vector;
 import java.util.Arrays;
 
 public class Vector {
-    private double[] array;
+    private double[] coordinates;
 
-    public Vector(int vectorSize) {
-        if (vectorSize <= 0) {
-            throw new IllegalArgumentException("Vector's size must be positive number: argument vectorSize = " + vectorSize);
+    public Vector(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Vector's size must be positive number: argument vectorSize = " + size);
         }
 
-        array = new double[vectorSize];
+        coordinates = new double[size];
     }
 
     public Vector(Vector vector) {
@@ -18,47 +18,40 @@ public class Vector {
             throw new NullPointerException("Constructor's argument must be not null");
         }
 
-        int size = vector.array.length;
-        array = new double[size];
-
-        System.arraycopy(vector.array, 0, array, 0, size);
+        coordinates = Arrays.copyOf(vector.coordinates, vector.coordinates.length);
     }
 
-    public Vector(double[] array) {
-        if (array == null) {
+    public Vector(double[] coordinates) {
+        if (coordinates == null) {
             throw new NullPointerException("Constructor's array argument must be not null");
         }
 
-        if (array.length == 0) {
+        if (coordinates.length == 0) {
             throw new IllegalArgumentException("Constructor's array argument length must be nonzero");
         }
 
-        this.array = new double[array.length];
-
-        System.arraycopy(array, 0, this.array, 0, array.length);
+        this.coordinates = Arrays.copyOf(coordinates, coordinates.length);
     }
 
-    public Vector(int vectorSize, double[] array) {
-        if (vectorSize <= 0) {
-            throw new IllegalArgumentException("Vector's size must be positive number: argument vectorSize = " + vectorSize);
+    public Vector(int size, double[] coordinates) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Vector's size must be positive number: argument vectorSize = " + size);
         }
 
-        if (array == null) {
+        if (coordinates == null) {
             throw new NullPointerException("Constructor's array argument must be not null");
         }
 
-        this.array = new double[vectorSize];
-
-        System.arraycopy(array, 0, this.array, 0, Math.min(array.length, vectorSize));
+        this.coordinates = Arrays.copyOf(coordinates, size);
     }
 
     public int getSize() {
-        return array.length;
+        return coordinates.length;
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(array).replace("[", "{").replace("]", "}");
+        return Arrays.toString(coordinates).replace("[", "{").replace("]", "}");
     }
 
     @Override
@@ -73,7 +66,7 @@ public class Vector {
 
         Vector v = (Vector) o;
 
-        return Arrays.equals(v.array, array);
+        return Arrays.equals(v.coordinates, coordinates);
     }
 
     @Override
@@ -81,34 +74,34 @@ public class Vector {
         final int prime = 37;
         int hash = 1;
 
-        hash = prime * hash + Arrays.hashCode(array);
+        hash = prime * hash + Arrays.hashCode(coordinates);
 
         return hash;
     }
 
     public void add(Vector vector) {
-        if (array.length < vector.array.length) {
-            array = Arrays.copyOf(array, vector.array.length);
+        if (coordinates.length < vector.coordinates.length) {
+            coordinates = Arrays.copyOf(coordinates, vector.coordinates.length);
         }
 
-        for (int i = 0; i < vector.array.length; i++) {
-            array[i] += vector.array[i];
+        for (int i = 0; i < vector.coordinates.length; i++) {
+            coordinates[i] += vector.coordinates[i];
         }
     }
 
     public void subtract(Vector vector) {
-        if (array.length < vector.array.length) {
-            array = Arrays.copyOf(array, vector.array.length);
+        if (coordinates.length < vector.coordinates.length) {
+            coordinates = Arrays.copyOf(coordinates, vector.coordinates.length);
         }
 
-        for (int i = 0; i < vector.array.length; i++) {
-            array[i] -= vector.array[i];
+        for (int i = 0; i < vector.coordinates.length; i++) {
+            coordinates[i] -= vector.coordinates[i];
         }
     }
 
     public void multiplyByScalar(double number) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] *= number;
+        for (int i = 0; i < coordinates.length; i++) {
+            coordinates[i] *= number;
         }
     }
 
@@ -119,7 +112,7 @@ public class Vector {
     public double getLength() {
         double elementsSquaresSum = 0;
 
-        for (double e : array) {
+        for (double e : coordinates) {
             elementsSquaresSum += e * e;
         }
 
@@ -127,23 +120,27 @@ public class Vector {
     }
 
     public double getCoordinate(int index) {
-        if (index < 0 || index >= array.length) {
-            throw new IndexOutOfBoundsException("Index must be non-negative and must be less than vector's size: index = " + index);
+        if (index < 0 || index >= coordinates.length) {
+            throw new IndexOutOfBoundsException(String.format(
+                    "Index must be non-negative and must be less than vector's size (%d): index = %d",
+                    coordinates.length, index));
         }
 
-        return array[index];
+        return coordinates[index];
     }
 
     public void setCoordinate(int index, double number) {
-        if (index < 0 || index >= array.length) {
-            throw new IndexOutOfBoundsException("Index must be non-negative and must be less than vector's size: index = " + index);
+        if (index < 0 || index >= coordinates.length) {
+            throw new IndexOutOfBoundsException(String.format(
+                    "Index must be non-negative and must be less than vector's size (%d): index = %d",
+                    coordinates.length, index));
         }
 
-        array[index] = number;
+        coordinates[index] = number;
     }
 
     public static Vector getSum(Vector vector1, Vector vector2) {
-        Vector result = new Vector(Math.max(vector1.array.length, vector2.array.length), vector1.array);
+        Vector result = new Vector(Math.max(vector1.coordinates.length, vector2.coordinates.length), vector1.coordinates);
 
         result.add(vector2);
 
@@ -151,7 +148,7 @@ public class Vector {
     }
 
     public static Vector getDifference(Vector vector1, Vector vector2) {
-        Vector result = new Vector(Math.max(vector1.array.length, vector2.array.length), vector1.array);
+        Vector result = new Vector(Math.max(vector1.coordinates.length, vector2.coordinates.length), vector1.coordinates);
 
         result.subtract(vector2);
 
@@ -161,10 +158,10 @@ public class Vector {
     public static double getScalarProduct(Vector vector1, Vector vector2) {
         double scalarProduct = 0;
 
-        int minVectorSize = Math.min(vector1.array.length, vector2.array.length);
+        int minVectorSize = Math.min(vector1.coordinates.length, vector2.coordinates.length);
 
         for (int i = 0; i < minVectorSize; i++) {
-            scalarProduct += vector1.array[i] * vector2.array[i];
+            scalarProduct += vector1.coordinates[i] * vector2.coordinates[i];
         }
 
         return scalarProduct;
